f6692b0f7d8e09d6ea1404691efab4c3493071c1
AndrewMagnaye <andrew.g.magnaye@gmail.com>
2018-07-02 19:50:48 -0400

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PostsComponent} from './posts/posts.component';
+import { AboutComponent} from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: PortfolioComponent
+  },
+  {
+    path:'posts',
+    component: PostsComponent
+  },
+  {
+    path:'about',
+    component: AboutComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { PostsComponent } from './posts/posts.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PortfolioComponent,
+    PostsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
